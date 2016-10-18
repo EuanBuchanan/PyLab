5991b0dffd33b08ef5cafa94072bd93551fe128f
Euan Buchanan <euan_b_oz@hotmail.com>
2016-10-17 23:15:11 -0700

    Started work on confparse excercise

---
+++ parse.py
@@ -0,0 +1,7 @@
+'''
+parses file for:
+    Blocks that start with 'crypto map CRYPTO'
+    All crypto map entries using PFS Group 2
+    All maps not using AES (based on the transform set name. Print these 
+        entries and their corresponding transform set name.
+'''
