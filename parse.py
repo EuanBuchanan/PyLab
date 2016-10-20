4f30644f2bbff3859a4e25f2c829f05d779fa1c5
Euan Buchanan <euan_b_oz@hotmail.com>
2016-10-19 23:35:51 -0700

    Fixiing before branching

--- parse.py
+++ parse.py
@@ -29,8 +29,67 @@ def match_block(filename, expression):
     parse = CiscoConfParse(filename)
     parse.find_blocks(expression)
     crypto_map_match_l = parse.find_blocks('crypto map CRYPTO')
+    print "\nBlocks matcing '" + expression + "'\n---\n"
     for line in crypto_map_match_l:
             print line
 
+def match_entries_w_expr(filename, parent, child):
+    '''
+    Fucntion that matches expression and prints parents contiaining them.
+
+    Pareamaters
+    ===========
+    filename : string, the name of the file. Path is relaitve to where the
+                scrit is called from
+    parent:  string, matches parent specs to be searched
+    child:  string, matches child specs to be searched
+
+    Returns
+    =======
+    None
+
+    Outputs
+    =======
+    Prints result to stdout
+    '''
+
+    parse = CiscoConfParse(filename)
+    expr_match_l = parse.find_parents_w_child(parent, child)
+    print "\nCrypto map entries containing '" + child + "'\n---\n"
+    for line in expr_match_l:
+        print line
+
+def match_entries_wo_expression(filename, parent, child):
+    '''
+    Function that maches expression and prints parents not containing them
+
+    Pareamaters
+    ===========
+    filename : string, the name of the file. Path is relaitve to where the
+                scrit is called from
+    parent:  string, matches parent specs to be searched
+    child:  string, matches child specs to be searched
+
+    Returns
+    =======
+    None
+
+    Outputs
+    =======
+    Prints result to stdout
+    '''
+
+    parse = CiscoConfParse(filename)
+    expr_match_l = parse.find_parents_wo_child(parent,child)
+    print "\nCrypto map entries not contiaing '" + child + "\n---\n"
+    for line in expr_match_l:
+        child_match = parse.find_children_w_parents(line,'transform-set')
+        print line
+        print child_match[0]
+
+
 if __name__ == '__main__':
     match_block('ciscoconfig','crypto map CRYPTO')
+    match_entries_w_expr('ciscoconfig', 'crypto map', 'pfs group2')
+    match_entries_wo_expression('ciscoconfig', 'crypto map',
+            'transform-set AES-SHA')
--- yamljsonwrite.py
+++ yamljsonwrite.py
@@ -4,27 +4,10 @@ keys.  Program writes this list out to file in both JSON and YAML format
 (using the expanded form)
 '''
 
-import logging.config
+import json
 import os
 import yaml
 
-def setup_logging(
-    default_path='access_6_logging.yaml',
-    default_level=logging.INFO,
-    env_key='LOG_CFG'
-    ):
-    """Setup logging configuration
-    """
-    path = default_path
-    value = os.getenv(env_key, None)
-    if value:
-        path = value
-    if os.path.exists(path):
-        with open(path, 'rt') as f:
-            config = yaml.safe_load(f.read())
-        logging.config.dictConfig(config)
-    else:
-        logging.basicConfig(level=default_level)
 
 
 def listwrite():
@@ -32,13 +15,9 @@ def listwrite():
     Writes list, including a dictionary with at least two keys
     '''
 
-    logger = logging.getLogger()
 
     the_list = [ 'Element', 1, ('x','y'), {"Luna" : "Princess",
         "Littlepip" : "Stable Dweller"} ]
-    logger.setLevel('DEBUG')
-    logger.debug('the_list: %s', the_list)
-    logger.setLevel('INFO')
     return the_list
 
 def yamldump():
@@ -49,10 +28,15 @@ def yamldump():
     with open('YAML_the_list.yaml', 'w') as outfile:
         yaml.dump(listwrite(), outfile, default_flow_style=False)
         
+def jsondump():
+    '''
+    Dumps list to json
+    '''
+    with open('JSON-the_list.json', 'w') as outfile:
+        json.dump(listwrite(), outfile)
 
 if __name__ == '__main__':
 
 
-    setup_logging()
     yamldump()
-
+    jsondump()
