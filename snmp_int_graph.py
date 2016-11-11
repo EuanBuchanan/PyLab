506752c4fe229fc88cc2a138e03f6e0a464cb600
Euan Buchanan <euan_b_oz@hotmail.com>
2016-11-10 21:03:30 -0800

    Wrote initial class __init__

--- snmp_int_graph.py
+++ snmp_int_graph.py
@@ -9,6 +9,39 @@ input/output octets transmitted during this five minute interval).
 The second SVG graph file should be the same as the first except graph the
 unicast packets received and transmitted
 '''
+import yaml
+from setup_logging import setup_logging
+import logging
+
+class Network_Devices(object):
+    '''
+    Class to hold snmp variables and methos
+    '''
+
+    def __init__(self, name, ip, snmp_username, aut_key, encrypt_key, snmp_port,
+            ifDescr_fa4, ifInOctets_fa4, ifUcastPkts_fa4, ifOutOctets_fa4,
+            ifOutUcastPkts_fa4):
+        self.name, self.ip, self.snmpusername = name, ip, snmp_username
+        self.aut_key, self.encrypt_key = aut_key, encrypt_key
+        self.snmp_port, self.ifDescr_fa4 = snmp_port, ifDescr_fa4
+        self.ifInOctets_fa4, self.ifUcastPkts_fa4 = ifInOctets_fa4, ifUcastPkts_fa4
+        self.ifOutOctets_fa4, self.ifOutUcastPkts_fa4 = ifOutOctets_fa4, ifUcastPkts_fa4
+
 
 def load_yaml():
+    '''
+    Loads per device information from yaml file
+    '''
+
+    logger = logging.getLogger('load_yaml')
+    with open('device_graph.yaml') as infile:
+        device_d = yaml.load(infile)
+    logger.info('device_d: %s', device_d)
+
+
+def main():
+    load_yaml()
+if __name__ == '__main__':
+    setup_logging()
+    main()
 
